ac298cc9204d13319b1634d86af95a543b762278
Charitha-Jeewanka <cjrathnayake24@gmail.com>
2023-08-21 18:33:23 +0530

    File Upload for GCP done

---
+++ ServerforFYP/Controllers/FileUploadController.cs
@@ -0,0 +1,34 @@
+﻿using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerforFYP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly StorageClient _storageClient;
+        public FileUploadController(StorageClient storageClient) 
+        {
+            _storageClient = storageClient;
+        }
+
+        [HttpPost(Name = "csvUpload")]
+        public async Task<IActionResult> UploadCsvFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            using (var stream = file.OpenReadStream())
+            {
+                var objectName = "biodata/" + file.FileName;
+                var bucketName = "uploads_for_fyp";
+                await _storageClient.UploadObjectAsync(bucketName, objectName, null, stream);
+            }
+
+            return Ok("File uploaded successfully.");
+        }
+    }
+}
--- ServerforFYP/Program.cs
+++ ServerforFYP/Program.cs
@@ -1,3 +1,6 @@
+using Google.Cloud.Storage.V1;
+using Google.Apis.Auth.OAuth2;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(provider =>
+{
+    var credentials = GoogleCredential.FromFile("D:\\FYP\\Server\\fypbackend1-d97d2809d29a.json");
+    return StorageClient.Create(credentials);
+});
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 
 var app = builder.Build();
 
@@ -15,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 
